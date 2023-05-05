fcf615f1e2b03e54744c150f79ba1c9248c31d2a
salihdeg <salihdegirmenci99@gmail.com>
2023-05-05 10:53:44 +0300

    Camera follow feature added

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private Transform _playerT;
+
+    private void LateUpdate()
+    {
+        
+    }
+
+}
