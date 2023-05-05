85a5e9ffdcdaf43e71728b9b41804b983a21b989
salihdeg <salihdegirmenci99@gmail.com>
2023-05-05 11:50:17 +0300

    Destroy method changed, Check Falling added

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,6 +11,7 @@ namespace Player
 
         private Vector3 _dir;
         private bool _dirChanged = false;
+        public static bool isDead = false;
 
         private readonly string GROUND_TAG = "Ground";
 
@@ -22,7 +23,20 @@ namespace Player
 
         private void Update()
         {
+            if (isDead) return;
+
             ChangeMoveDirection();
+
+            CheckFalling();
+        }
+
+        private void CheckFalling()
+        {
+            if (transform.position.y < 0.2f)
+            {
+                isDead = true;
+                Destroy(gameObject, 2f);
+            }
         }
 
         private void FixedUpdate()
@@ -65,7 +79,10 @@ namespace Player
 
         private IEnumerator DestroyGround(GameObject ground)
         {
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(.3f);
+            ground.AddComponent<Rigidbody>();
+
+            yield return new WaitForSeconds(1.5f);
             Destroy(ground);
         }
 
