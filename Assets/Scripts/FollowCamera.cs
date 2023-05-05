7dffc6d7799179e7c91b5178a1ddcc4c18c024f6
salihdeg <salihdegirmenci99@gmail.com>
2023-05-05 11:05:43 +0300

    Update FollowCamera.cs

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     [SerializeField] private Transform _playerT;
+    [SerializeField] private Vector3 _camDistance;
+
+    private void Start()
+    {
+        _camDistance = transform.position - _playerT.position; // distance between camera and player
+    }
 
     private void LateUpdate()
     {
-        
+        transform.position = _playerT.position + _camDistance;
     }
 
 }
