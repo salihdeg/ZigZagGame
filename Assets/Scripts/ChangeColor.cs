5f6665dfff9866c02104c7eb37ce6e6d0e591be7
salihdeg <salihdegirmenci99@gmail.com>
2023-05-05 15:51:49 +0300

    Color Change Edited

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -7,14 +7,40 @@ public class ChangeColor : MonoBehaviour
     [SerializeField] private Color[] _colors;
 
     private Color _firstColor, _secondColor;
-    private int _colorIndex;
+    private int _colorIndex, _secondColorIndex;
 
     [SerializeField] private Material _groundMat;
 
     private void Start()
     {
         _colorIndex = Random.Range(0, _colors.Length);
+        _secondColorIndex = SelectSecondColor();
+        _secondColor = _colors[_secondColorIndex];
         _groundMat.color = _colors[_colorIndex];
-        Camera.main.backgroundColor = _colors[_colorIndex];
+        Camera.main.backgroundColor = _secondColor;
+    }
+
+    private void Update()
+    {
+        Color diff = _groundMat.color - _secondColor;
+
+        if (Mathf.Abs(diff.r) + Mathf.Abs(diff.g) + Mathf.Abs(diff.b) < 0.1f)
+        {
+            _secondColor = _colors[SelectSecondColor()];
+        }
+
+        _groundMat.color = Color.Lerp(_groundMat.color, _secondColor, 0.003f);
+        Camera.main.backgroundColor = Color.Lerp(Camera.main.backgroundColor, _secondColor, 0.002f);
+    }
+
+    private int SelectSecondColor()
+    {
+        int secondColorIndex = Random.Range(0, _colors.Length);
+        while (secondColorIndex == _colorIndex)
+        {
+            secondColorIndex = Random.Range(0, _colors.Length);
+        }
+
+        return secondColorIndex;
     }
 }
