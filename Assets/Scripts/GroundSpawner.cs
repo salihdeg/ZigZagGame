b7b1b65d0ca5dd5656f5950263b1311875734803
salihdeg <salihdegirmenci99@gmail.com>
2023-05-05 10:24:30 +0300

    Default level designed

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class GroundSpawner : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject _sonZemin;
+
+    private void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            CreateGround();
+        }
+    }
+
+    private void CreateGround()
+    {
+        _sonZemin = Instantiate(_sonZemin, _sonZemin.transform.position + Vector3.forward, _sonZemin.transform.rotation);
+    }
 }
